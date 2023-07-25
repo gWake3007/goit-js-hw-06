a36cec3e8fa06d12c2f57bc7eea29d3c1683869d
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-07-25 08:42:00 +0200

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,33 @@
-// const categ = document.getElementById("categories");
-// console.log(categ);
+// Number of categories
 
+// const categories = document.querySelector("#categories");
+// const liItem = categories.querySelectorAll("li.item");
+// const itemLength = `Number of categories: ${liItem.length}`;
+// console.log(itemLength);
 
-const categ = document.querySelector("ul#categories");
-console.log(categ);
+const categoriesUl = document.querySelector("#categories");
+console.log(`Number of categories: ${categoriesUl.children.length}`);
+
+// All categories
+// const title = document.querySelector("h2").textContent;
+// console.log(`Category: ${title}`);
+
+// title.forEach(titles => {
+//     categoriesMass.push(`Category: ${titles.textContent}`)
+// });
+
+// const categoriesList = document.querySelectorAll("ul#categories .item ul");
+
+// const categoriesMass = [];
+
+// categoriesList.forEach(elem => {
+//     categoriesMass.push(`Elements: ${elem.children.length}`)
+// });
+// console.log(categoriesMass.join(" "));
+
+[...categoriesUl.children].forEach((elem) =>{
+    const categorisTitle = elem.querySelector("h2");
+    const categoriesNumber = elem.querySelectorAll("li");
+    console.log(`Category: ${categorisTitle.textContent}`);
+    console.log(`Elements: ${categoriesNumber.length}`);
+});
