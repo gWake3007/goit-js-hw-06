47c7e6af6797ce73bbf135445921770c56050d21
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-07-31 22:18:19 +0200

    1

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,9 +1,17 @@
-const input = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
+// const input = document.querySelector("#font-size-control");
+// const text = document.querySelector("#text");
 
-// input.setAttribute(text.style.fontSize, 90);
+// // input.setAttribute(text.style.fontSize, 90);
 
-input.addEventListener("input", skroll);
-function skroll(e) {
-    text.style.fontSize = `${e.target.value}px`;
-}
+// input.addEventListener("input", skroll);
+// function skroll(e) {
+//     text.style.fontSize = `${e.target.value}px`;
+// }
+
+
+const inputField = document.querySelector(`#font-size-control`);
+const text = document.querySelector(`#text`);
+inputField.addEventListener(`input`, () => {
+    const fontSize = inputField.value + `px`;
+    text.style.fontSize = fontSize;
+})
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" && password.value === "") {
+        return alert('Всі поля повинні бути заповнені!');
+    }
+    else{
+console.log(`Email: ${email.value}, Password: ${password.value}`);
+           
+    }
+    event.currentTarget.reset(); 
+}
