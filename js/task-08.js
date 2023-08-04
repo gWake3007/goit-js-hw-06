b51cb9805097c0999cc65079fc635b429a17b903
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-04 15:50:47 +0200

    1234

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,44 @@
-const form = document.querySelector(".form");
+// const form = document.querySelector(".form");
 
-form.addEventListener("submit", handleSubmit);
+// form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const {
-        elements: { email, password }
-    } = event.currentTarget;
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const {
+//         elements: { email, password }
+//     } = event.currentTarget;
 
-    if (email.value === "" && password.value === "") {
-        return alert('Всі поля повинні бути заповнені!');
-    }
-    else{
-console.log(`Email: ${email.value}, Password: ${password.value}`);
+//     if (email.value === "" || password.value === "") {
+//         return alert('Всі поля повинні бути заповнені!');
+//     }
+//     else{
+// console.log(`Email: ${email.value}, Password: ${password.value}`);
            
-    }
-    event.currentTarget.reset(); 
-}
+//     }
+//     event.currentTarget.reset(); 
+// }
+
+
+const formElement = document.querySelector('.login-form');
+formElement.addEventListener('submit', onFormSubmit);
+
+console.log(formElement);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const { email, password } = event.currentTarget.elements
+    console.log(email.value)
+    console.log(password.value)
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(data);
+
+    if (email.value === "" || password.value === "") {
+        alert(`Не всі поля заповненні!`);
+    };
+
+};
